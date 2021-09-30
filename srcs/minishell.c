e44b6cb2775601b25e05f1d8324cf48384707282
Marc-andre Fortin <mafortin@c2r3p4.42quebec.com>
2021-09-30 19:19:42 -0400

    EXEC: CD & PWD working + tested

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 18:25:12 by mafortin          #+#    #+#             */
-/*   Updated: 2021/09/30 19:12:10 by mafortin         ###   ########.fr       */
+/*   Updated: 2021/09/30 19:19:23 by mafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,14 @@
 void	ms_readline_loop(char **envp_ms)
 {
 	char	*input;
-	char	**cmd;
 
-	cmd = malloc(sizeof(char *) * 2);
-	cmd[0] = malloc(sizeof(char) * 3);
-	ft_strlcpy(cmd[0], "cd", 3);
 	signal(SIGINT, ms_nl_signal);
 	signal(SIGQUIT, SIG_IGN);
 	while (true)
 	{
 		input = readline("MiniShell % ");
 		//parse_input(input);
-		if (ft_strncmp(input, "pwd", 3) == 0)
-			ms_pwd_main();
-		if (ft_strncmp(input, "cd", 2) == 0)
-			ms_cd_main(cmd, envp_ms);
-		if (ft_strncmp(input, "exit", 4) == 0)
-		{
-			free(input);
-			ft_free_tab(envp_ms);
-			ft_free_tab(cmd);
-			exit(0);
-		}
-
-		//fonction qui recoit l'input (PARSING)
-		//fonction qui exec avec la linked list global
+		//ms_exec_main(t_job *job_head);
 		free (input);
 	}
 }
@@ -48,10 +31,8 @@ void	ms_readline_loop(char **envp_ms)
 int	main(int argc, char **argv, char **envp)
 {
 	char	**envp_ms = NULL;
-	//t_job	*cmd_head;//structure-linkedlist peupler par le parsing et utliser pour l'execution
+	//t_job	*job_head;//structure-linkedlist peupler par le parsing et utliser pour l'execution
 	//cree et placer ici une fonction pour bien init la linked list (EXEC OU PARSING)
-	argv++;
-	argc++;
 	envp_ms = ft_matrice_cpy(envp);
 	ms_readline_loop(envp_ms);
 	ft_free_tab(envp_ms);
