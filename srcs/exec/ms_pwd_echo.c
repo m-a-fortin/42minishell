0d1f8a1e661d2e534dd2ccabb10e8426ce3d8de4
Marc-andre Fortin <mafortin@c2r3p4.42quebec.com>
2021-09-30 20:34:23 -0400

    EXEC: echo done, need testing

--- srcs/exec/ms_pwd_echo.c
+++ srcs/exec/ms_pwd_echo.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_pwd.c                                           :+:      :+:    :+:   */
+/*   ms_pwd_echo.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 14:31:21 by mafortin          #+#    #+#             */
-/*   Updated: 2021/09/30 19:48:32 by mafortin         ###   ########.fr       */
+/*   Updated: 2021/09/30 20:33:32 by mafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,43 @@ void	ms_pwd_main(void)
 	pwd_line = getcwd(pwd_line, 0);
 	ft_putendl_fd(pwd_line, 1);
 	free(pwd_line);
+}
+
+void	ms_echo_loop(char **args, int index, int no_nl)
+{
+	while (args[index])
+	{
+		if (no_nl == 1)
+			ft_putstr_fd(args[index], 1);
+		else
+			ft_putendl_fd(args[index], 1);
+		index++;
+	}
+}
+
+void	ms_echo_main(char **args)
+{
+	int	argc;
+	int	no_nl;
+	int	index;
+
+	no_nl = 0;
+	index = 1;
+	argc = ft_matrice_size(args);
+	if (argc == 1)
+	{
+		ft_putendl_fd("\n", 1);
+		return ;
+	}
+	if (ft_strncmp(args[index], "-n", 2) == 0)
+	{
+		if (argc == 2)
+		{
+			ft_putchar_fd('\n', 1);
+			return ;
+		}
+		index++;
+		no_nl++;
+	}
+	ms_echo_loop(args, index, no_nl);
 }
