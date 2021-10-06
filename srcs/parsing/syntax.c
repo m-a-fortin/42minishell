54e3ba50a9295e4a053fade1d10917b5ac082447
mmondell <mmondell@student.42quebec.com>
2021-10-06 11:11:36 -0400

    Confirmed make linux success

--- srcs/parsing/syntax.c
+++ srcs/parsing/syntax.c
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 09:56:11 by mmondell          #+#    #+#             */
-/*   Updated: 2021/10/06 10:27:53 by mmondell         ###   ########.fr       */
+/*   Updated: 2021/10/06 11:10:54 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ bool	validate_tokens_syntax(t_token *head)
 	{
 		if (token->type == PIPE)
 		{
-			print_error(NAME, UNXP_TOKEN_PIPE, BAD_SYNTAX);
+			print_error(NAME, UNXP_TOKEN_PIPE);
 			return (false);
 		}
 		//if (!validate_command(token) || !validate_redir(token))
