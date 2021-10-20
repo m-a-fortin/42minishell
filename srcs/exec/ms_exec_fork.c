cc21fabbb16164a0bd7ff9a8fe0edef0c9deda26
Marc-Andre Fortin <mafortin@student.42quebec.com>
2021-10-20 11:51:39 -0400

    working on exec fork

--- srcs/exec/ms_exec_fork.c
+++ srcs/exec/ms_exec_fork.c
@@ -3,29 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ms_exec_fork.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hpst <hpst@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:44:10 by mafortin          #+#    #+#             */
-/*   Updated: 2021/10/19 11:30:33 by hpst             ###   ########.fr       */
+/*   Updated: 2021/10/20 11:24:53 by mafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+bool	ms_cmdispath(char **cmd)
+{
+	struct stat info;
+	stat(cmd[0], &info);
+	if (S_ISDIR(info.st_mode))
+	{
+		ft_putstr_fd("Minishell: ", 1);
+		ft_putstr_fd(cmd[0], 1);
+		ft_putendl_fd(": is a directory", 1);
+		return (false);
+	}
+	
+}
+
 bool	ms_fork(char **cmd)
 {
 	char	*path;
-	char	*cmd_name;
 
 	path = NULL;
 	if (execve(cmd[0], cmd, g_ms.env) == -1)
 	{
-		cmd_name = ft_strjoin("/", cmd);
-		if (ft_char_search(cmd[0], '/') == 0)
-			path = ms_get_cmdpath(cmd_name, cmd[0]);
-		else
-			path = ft_strdup(cmd[0]);
-		free (cmd_name);
+		if (ft_char_search(cmd[0], '/') > 0)
+			return (ms_cmdispath(cmd));
+		path = ms_get_cmdpath(cmd[0]);
 		if (!path)
 			return (ms_execve_error());
 	}
@@ -35,8 +45,11 @@ bool	ms_exec_fork(t_job *current, t_job *job_head, bool pipe)
 {
 	
 	pid_t	pid;
+	int		status;
 
 	pid = fork();
+	signal(SIGINT, NULL);
+	signal(SIGQUIT, NULL);
 	if (pid == -1)
 	{
 		ms_return_fd();
@@ -45,7 +58,11 @@ bool	ms_exec_fork(t_job *current, t_job *job_head, bool pipe)
 	}
 	if (pid == 0)
 	{
-		if (ms_check_builtin(current, job_head, 1) == false)
-			ms_fork(current->cmd);
+		if (pipe = true)
+			if (ms_check_builtin(current, job_head, 1) == false)
+				return (ms_fork(current->cmd));
 	}
+	waitpid(pid, &status, 0);
+	signal(SIGINT, ms_nl_signal);
+	signal(SIGQUIT, SIG_IGN);
 }
