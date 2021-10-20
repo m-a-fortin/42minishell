b635182b0f629d2c609be5bbefb90ba0a2c5155b
Marc-Andre Fortin <mafortin@student.42quebec.com>
2021-10-20 10:58:07 -0400

    fixed cd and oldpwd

--- srcs/exec/ms_cd.c
+++ srcs/exec/ms_cd.c
@@ -6,16 +6,24 @@
 /*   By: mafortin <mafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 11:45:54 by mafortin          #+#    #+#             */
-/*   Updated: 2021/10/08 14:10:07 by mafortin         ###   ########.fr       */
+/*   Updated: 2021/10/20 10:56:48 by mafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	ms_cd_update_env(char *old_pwd)
+{
+	g_ms.env = ms_setenv("PWD=", getcwd(NULL, 0), g_ms.env);
+	g_ms.env = ms_setenv("OLDPWD=", old_pwd, g_ms.env);
+	free (old_pwd);
+}
+
 //cd dans home si il est set. Si unset retourne un message d'erreur.
 void	ms_cd_home(void)
 {
 	char	*new_pwd;
+	char	*old_pwd;
 
 	new_pwd = ms_getenv("HOME=", g_ms.env);
 	if (new_pwd == NULL)
@@ -23,7 +31,9 @@ void	ms_cd_home(void)
 		ft_putstr_fd("minishell: cd: HOME not set\n", 1);
 		return ;
 	}
+	old_pwd = ft_strdup(getcwd(NULL, 0));
 	chdir(new_pwd);
+	ms_cd_update_env(old_pwd);
 }
 
 //Si le path est bon, chdir dans le path et update l'envp_ms.
@@ -31,8 +41,10 @@ void	ms_cd_home(void)
 int	ms_cd_main(char **cmd)
 {
 	char	*error;
+	char	*old_pwd;
 
 	error = 0;
+	old_pwd = ft_strdup(getcwd(NULL, 0));
 	if (cmd[1] == NULL || ft_strncmp(cmd[1], "~", 1) == 0)
 		ms_cd_home();
 	else if (chdir(cmd[1]) == -1)
@@ -45,6 +57,6 @@ int	ms_cd_main(char **cmd)
 		return (1);
 	}
 	else
-		g_ms.env = ms_setenv("PWD=", cmd[1], g_ms.env);
+		ms_cd_update_env(old_pwd);
 	return (0);
 }
